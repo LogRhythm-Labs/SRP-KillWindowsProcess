73d8f3739abd0c4ea54720e69654390ff30fc2e8
Matt Willems <matt.willems@logrhythm.com>
2016-04-01 13:57:50 -0600

    Initial Commit

---
+++ KillProcess.ps1
@@ -0,0 +1,62 @@
+# Set up a trap to properly exit on terminating exceptions
+trap [Exception] {
+	write-error $("TRAPPED: " + $_)
+	exit 1
+	}
+
+$TargetHost = $args[0]
+
+# Determine if process name or PID was passed and set up proper variables
+# If PID is from Win 2008, it will be in hex, so convert to decimal before passing along
+
+if($args[1] -match '^0x\w+$') {
+$HexProcess = $args[1]
+$TargetProcess = [Convert]::ToInt32($HexProcess,16)
+$filter = "ProcessID"
+$operator = "="
+$wildcared = ""
+}
+
+elseif($args[1] -match '^\d+$') {
+$TargetProcess = $args[1]
+$filter = "ProcessID"
+$operator = "="
+$wildcared = ""
+}
+
+else {
+$filter = "Name"
+$operator = "LIKE"
+$TargetProcess = $args[1]
+$wildcard = "%"
+}
+
+
+# Get local hostname and IP to determine whether action will be local or remote
+$localHost = [System.Net.Dns]::GetHostName()
+$localIP = [System.Net.Dns]::GetHostAddresses("$localHost")
+
+# If local, or no credentials passed, build appropriate WMI object
+# if it is remote and no cred were passed, commands will be run under current credentials
+if($localHost -like $TargetHost -OR $localIP -like $TargetHost -OR -not $args[2]) {
+$process = Get-WMIObject -Class Win32_Process -Filter "$filter $operator '$TargetProcess$wildcard'" -ComputerName "$TargetHost" | % {$_.Terminate() }
+				}
+
+# If remote and alternative credentials passed, build modified WMI object with credentials
+else {
+$username = $args[2]
+$password = $args[3]
+$securePassword = new-Object System.Security.SecureString
+$password.ToCharArray() | % { $securePassword.AppendChar($_) }
+$credential = New-Object -typename System.Management.Automation.PSCredential -argumentlist "$username",$securePassword
+$process = Get-WMIObject -Class Win32_Process -Filter "$filter $operator '$TargetProcess$wildcard'" -ComputerName "$TargetHost" -Credential $credential | % {$_.Terminate() }
+}
+
+
+# Handle any non-terminating exceptions
+if(-not $?)	{
+		Write-Error "Unable to get WMI Object"
+		exit 1
+		}
+
+Write-Host "Process $TargetProcess on $TargetHost killed"
